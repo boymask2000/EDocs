a25eca78775274e3913c08b06bead59430793ee4
Giovanni Posabella <g.posabella@it.ibm.com>
2021-08-17 10:35:44 +0200

    y

--- app/src/main/java/com/boymask/edocs/SettingsActivity.java
+++ app/src/main/java/com/boymask/edocs/SettingsActivity.java
@@ -1,10 +1,17 @@
 package com.boymask.edocs;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.EditTextPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -22,12 +29,37 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+            EditTextPreference server =
+                    (EditTextPreference) findPreference("server");
+
+            server.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                   preference.setDefaultValue((String) newValue);
+              //      preference.callChangeListener(newValue);
+                    return true;
+                }
+            });
+            /*
+            server.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+                @Override
+                public void onBindEditText(@NonNull EditText editText) {
+                    editText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED);
+
+                }
+            });
+            */
+
         }
     }
 }
